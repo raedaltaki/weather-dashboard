43b2c3f396e626fe6151d99d49e3344dd9959362
raedaltaki <raed.simon@gmail.com>
2021-01-09 15:13:18 -0500

    limit search history to 10 and add icon to weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ var searchHistory = [];
 
 var loadSearchHistory = function()
 {
+    searchHistoryListEl.innerHTML= "";
     searchHistory = localStorage.getItem("history");
     if (searchHistory)
     {
@@ -20,43 +21,25 @@ var loadSearchHistory = function()
     {
         searchHistory = [];
     }
-
+    var historylimit =10;
     for(var i = searchHistory.length-1 ; i>-1; i--)
     {
         var newSearch = document.createElement("li");
         newSearch.className = "list-group-item"
         newSearch.textContent = searchHistory[i];
         searchHistoryListEl.appendChild(newSearch);
+        historylimit--;
+        if(historylimit===0)
+        {
+            break;
+        }
     }
-};
-
-var addSearchHistory = function()
-{
-    //load search history from local storage
-    var searchInput = searchInputEl.value;
-    searchInputEl.value = "";
-    if(searchInput)
-    {
-        searchHistory.push(searchInput);
-        var newSearch = document.createElement("li");
-        newSearch.className = "list-group-item"
-        newSearch.textContent = searchInput;
-        searchHistoryListEl.insertBefore(newSearch, searchHistoryListEl.firstChild);
-    }
-    else
-    {
-        window.alert("Please Enter Search City !!!");
-    }
-    //save search history in local storage
-    saveSearchHistory();
 };
 
 var getWeatherDetails = function()
 {
-    var today = moment().format("MM/DD/YY");
-    //today = today.toString();
     var country = searchInputEl.value.trim();
-    var weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=274cbbc7cb2cf2adbf2edf074233aaec&q=" + country;
+    var weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=274cbbc7cb2cf2adbf2edf074233aaec&units=imperial&q=" + country;
     fetch(weatherURL)
         .then(function(response)
         {
@@ -64,59 +47,17 @@ var getWeatherDetails = function()
             {
                 response.json().then(function(data)
                 {
-                    weatherHeaderEl.textContent = data.name + " (" + today + ")"; 
-                    var temp =(parseFloat(data.main.temp) - 273.15) * 9/5 + 32;
-                    tempretureEl.textContent =" " + temp.toFixed(2) + " °F";
-                    humidityEl.textContent =" " + data.main.humidity + " %";
-                    windSpeedEl.textContent =" " + data.wind.speed + " m/s";
+                    var today = moment().format("MM/DD/YY");
+                    weatherHeaderEl.textContent = data.name + " (" + today + ")";
+
+                    var weathericonEl= document.createElement("img");
+                    weathericonEl.src = "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
+                    weatherHeaderEl.appendChild(weathericonEl);
+
                     var lat= data.coord.lat;
                     var lon = data.coord.lon;
-                    fetch("http://api.openweathermap.org/data/2.5/uvi?appid=274cbbc7cb2cf2adbf2edf074233aaec&lat="+lat+"&lon="+lon)
-                        .then(function(response){
-                            if(response.ok)
-                            {
-                                response.json().then(function(data)
-                                {
-                                    uvIndexEl.textContent =" " + data.value;
-                                    uvIndexEl.style.color = "white";
-                                    var uvindex = parseInt(data.value);
-                                    console.log(data.value,uvindex);
-
-                                    switch(uvindex)
-                                    {
-                                        case 0:
-                                        case 1:
-                                        case 2:
-                                            uvIndexEl.style.backgroundColor = "green";
-                                            break;
-                                        case 3:
-                                        case 4:
-                                        case 5:
-                                            uvIndexEl.style.backgroundColor = "yellow";
-                                            break;
-                                        case 6:
-                                        case 7:
-                                            uvIndexEl.style.backgroundColor = "orange";
-                                            break;
-                                        case 8:
-                                        case 9:
-                                        case 10:
-                                            uvIndexEl.style.backgroundColor = "yellow";
-                                            break;
-                                        default:
-                                            uvIndexEl.style.backgroundColor = "purple";
-                                            break;
-                                    }
-                                })
-                            }
-                            else
-                            {
-
-                            }
-                        })
-                        .catch(function(error){
-
-                        })
+                    getTodayWeather(lat,lon);
+                    getWeatherForecast(lat,lon);
                 })
             }
             else
@@ -128,37 +69,104 @@ var getWeatherDetails = function()
         {
             alert("22222222");
         })
-        getWeatherForecast();
-        addSearchHistory();
-}
 
-var getWeatherForecast = function()
+        addSearchHistory();
+};
+
+var getTodayWeather = function(lat,lon)
 {
-    var country = searchInputEl.value.trim();
-    var url = "http://api.openweathermap.org/data/2.5/forecast?appid=274cbbc7cb2cf2adbf2edf074233aaec&q=" + country;
-    fetch(url)
+    var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely&units=imperial&appid=274cbbc7cb2cf2adbf2edf074233aaec&lat="+lat+"&lon="+lon;
+    fetch(weatherURL)
         .then(function(response)
         {
-            response.json().then(function(data)
+            if(response.ok)
             {
-                for(var i = 1; i<6; i++)
+                response.json().then(function(data)
                 {
-                    var forecastEl = document.querySelector('*[data-forecase="day'+i+'"]');
+                    tempretureEl.textContent =" " + data.current.temp + " °F";
+                    humidityEl.textContent =" " + data.current.humidity + " %";
+                    windSpeedEl.textContent =" " + data.current.wind_speed + " m/s";
+                    uvIndexEl.textContent =" " + data.current.uvi;
+                    uvIndexEl.style.color = "white";
+                    var uvindex = parseInt(data.current.uvi);
+    
+                    switch(uvindex)
+                    {
+                        case 0:
+                        case 1:
+                        case 2:
+                            uvIndexEl.style.backgroundColor = "green";
+                            break;
+                        case 3:
+                        case 4:
+                        case 5:
+                            uvIndexEl.style.backgroundColor = "yellow";
+                            break;
+                        case 6:
+                        case 7:
+                            uvIndexEl.style.backgroundColor = "orange";
+                            break;
+                        case 8:
+                        case 9:
+                        case 10:
+                            uvIndexEl.style.backgroundColor = "yellow";
+                            break;
+                        default:
+                            uvIndexEl.style.backgroundColor = "purple";
+                            break;
+                    }
+                })
+            }
+            else
+            {
+                alert("1111111");
+            }
+        })
+        .catch(function(error)
+        {
+            alert("22222222");
+        })
 
-                    var forecastDateEl = document.createElement("h5");
-                    forecastDateEl.textContent = moment().add(i,'days').format("MM/DD/YY");
-                    forecastEl.appendChild(forecastDateEl);
+}
 
-                    var forecastTempEl = document.createElement("p");
-                    forecastTempEl.textContent = "Temp: " + data.list[i].main.temp + " °F";
-                    forecastEl.appendChild(forecastTempEl);
+var getWeatherForecast = function(lat,lon)
+{
+    var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely&units=imperial&appid=274cbbc7cb2cf2adbf2edf074233aaec&lat="+lat+"&lon="+lon;
+    fetch(weatherURL)
+        .then(function(response)
+        {   if(response.ok)
+            {
+                response.json().then(function(data)
+                {
+                    for(var i = 1; i<6; i++)
+                    {
+                        var forecastEl = document.querySelector('*[data-forecase="day'+i+'"]');
+                        forecastEl.innerHTML= "";
 
-                    var forecastHumidityEl = document.createElement("p");
-                    forecastHumidityEl.textContent = "Humidity: " + data.list[i].main.humidity + " %";
-                    forecastEl.appendChild(forecastHumidityEl);
-                }
+                        var forecastDateEl = document.createElement("h5");
+                        forecastDateEl.textContent = moment(data.daily[i].dt,"X").format("MM/DD/YY");
+                        forecastEl.appendChild(forecastDateEl);
 
-            })
+                        var forecastIconEl = document.createElement("img");
+                        forecastIconEl.src = "http://openweathermap.org/img/wn/"+data.daily[i].weather[0].icon+"@2x.png";
+                        forecastEl.appendChild(forecastIconEl);
+
+                        var forecastTempEl = document.createElement("p");
+                        forecastTempEl.textContent = "Temp: " + data.daily[i].temp.day + " °F";
+                        forecastEl.appendChild(forecastTempEl);
+
+                        var forecastHumidityEl = document.createElement("p");
+                        forecastHumidityEl.textContent = "Humidity: " + data.daily[i].humidity + " %";
+                        forecastEl.appendChild(forecastHumidityEl);
+                        
+                    }
+
+                })
+            }
+            else
+            {
+
+            }
         })
         .catch(function(error)
         {
@@ -167,10 +175,27 @@ var getWeatherForecast = function()
 
 };
 
+var addSearchHistory = function()
+{
+    //load search history from local storage
+    var searchInput = searchInputEl.value;
+    searchInputEl.value = "";
+    if(searchInput)
+    {
+        searchHistory.push(searchInput);
+        saveSearchHistory();
+        loadSearchHistory();
+    }
+    else
+    {
+        window.alert("Please Enter Search City !!!");
+    }
+};
+
 var saveSearchHistory = function()
 {
     localStorage.setItem("history",searchHistory);
-}
+};
 
 
 
